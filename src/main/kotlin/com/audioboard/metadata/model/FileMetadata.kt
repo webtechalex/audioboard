bbdbc50427a32a00af0f5de538ca1b0829f6a641
alexanderbrown <alexanderbrown.info@gmail.com>
2023-12-11 13:32:48 +0000

    map the model to the database table

--- src/main/kotlin/com/audioboard/metadata/model/FileMetadata.kt
+++ src/main/kotlin/com/audioboard/metadata/model/FileMetadata.kt
@@ -1,8 +1,10 @@
 package com.audioboard.metadata.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
 
+@Table("file_metadata")
 data class FileMetadata(
     @Id
     val id: UUID,
