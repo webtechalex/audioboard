6f6da10483f82bbf38fd0a5a1c9312661cd646da
alexanderbrown <alexanderbrown.info@gmail.com>
2023-12-13 13:31:45 +0000

    add findByOwner query

--- src/main/kotlin/com/audioboard/metadata/repository/FileMetadataRepository.kt
+++ src/main/kotlin/com/audioboard/metadata/repository/FileMetadataRepository.kt
@@ -1,8 +1,12 @@
 package com.audioboard.metadata.repository
 
 import com.audioboard.metadata.model.FileMetadata
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
-import java.util.UUID
+import org.springframework.data.repository.query.Param
+import java.util.*
 
 interface FileMetadataRepository : CrudRepository<FileMetadata, UUID> {
+    @Query("SELECT * FROM FILE_METADATA WHERE owner = :owner")
+    fun findByOwner(@Param("owner") owner: UUID?): List<FileMetadata?>?
 }
