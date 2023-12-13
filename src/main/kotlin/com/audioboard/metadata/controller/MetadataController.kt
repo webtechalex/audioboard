2dbee89bdfac85574f608c1a8ac58f43e17b0f09
alexanderbrown <alexanderbrown.info@gmail.com>
2023-12-13 13:32:52 +0000

    add getMetaData endpoint

--- src/main/kotlin/com/audioboard/metadata/controller/MetadataController.kt
+++ src/main/kotlin/com/audioboard/metadata/controller/MetadataController.kt
@@ -1,7 +1,21 @@
 package com.audioboard.metadata.controller
 
-import org.springframework.stereotype.Controller
+import com.audioboard.metadata.service.MetadataService
+import com.audioboard.metadata.model.FileMetadata
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class MetadataController {
+@RestController
+@RequestMapping("/metadata")
+class MetadataController(val metadataService: MetadataService) {
+
+    @GetMapping("/{id}")
+    fun getMetadata(@PathVariable id: String): ResponseEntity<FileMetadata> {
+        val metadata = metadataService.getMetadataByUserId(id)
+        return if (metadata == null) ResponseEntity.notFound().build()
+        else ResponseEntity.ok(metadata)
+    }
 }
